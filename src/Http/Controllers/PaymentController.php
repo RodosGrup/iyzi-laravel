9f68a98a5c5170828de31e6d67d61565932d3626
1batu <32592602+1batu@users.noreply.github.com>
2023-07-21 15:36:23 +0300

    3Ds create back

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -3,11 +3,28 @@
 namespace RodosGrup\IyziLaravel\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Iyzipay\Model\Locale;
+use Iyzipay\Model\ThreedsPayment;
+use Iyzipay\Options;
+use Iyzipay\Request\CreateThreedsPaymentRequest;
 
 class PaymentController extends Controller
 {
     public function return(Request $request)
     {
+        $options = new Options();
+        $options->setBaseUrl(config('iyzi-laravel.baseUrl'));
+        $options->setApiKey(config('iyzi-laravel.apiKey'));
+        $options->setSecretKey(config('iyzi-laravel.secretKey'));
+
+        $pay = new CreateThreedsPaymentRequest();
+        $pay->setLocale(Locale::TR);
+        $pay->setConversationId($request->conversationId);
+        $pay->setPaymentId($request->paymentId);
+        $pay->setConversationData($request->conversationData);
+
+        $threedsPayment = ThreedsPayment::create($pay, $options);
+
         return redirect()->route('iyzico.laravel.gateway')
             ->with([
                 'content' => $request->all()
